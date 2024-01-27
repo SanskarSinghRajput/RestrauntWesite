038b36ef03f838b3fa28954341f0fdd35b04b2b0
SanskarSinghRajput <you@example.com>
2024-01-28 03:04:28 +0530

    "First com it"

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='max-w-[1640px]'>
+      <div className='flex justify-center text-center'>
+         <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>
+           Best <span className='font-bold'>Eats</span>
+         </h1>
+         <p className='mt-auto'>all rights reserved</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
